9ddaef173d5293a02e05a7d99c0664c3de8fa9a4
Christopher <cristoferperezmejia80@gmail.com>
2021-09-16 12:32:13 -0400

    data added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,38 @@
 import Dashboard from "./Dashboard/Dashboard";
 
 const data = [
-  { socialMedia: "Facebook", followers: 1987, increment: 12, decrement: false },
-  { socialMedia: "Twitter", followers: 1044, increment: 99, decrement: false },
-  { socialMedia: "Instagram", followers: "11k", increment: 1099 },
-  { socialMedia: "Youtube", followers: 8239, increment: false, decrement: 144 },
+  { socialMedia: "Facebook", followers: "1987", increment: "12", decrement: false },
+  { socialMedia: "Twitter", followers: "1044", increment: "99", decrement: false },
+  { socialMedia: "Instagram", followers: "11k", increment: "1099" },
+  { socialMedia: "Youtube", followers: "8239", increment: false, decrement: "144" },
+];
+
+const dailyIncomingData = [
+  { socialMedia: "Facebook", pageViews: "87", increment: "3%", decrement: false, likes: { likes: "52", increment: false, decrement: "2%" } },
+  {
+    socialMedia: "Instagram",
+    profileViews: "52k",
+    increment: "1375%",
+    decrement: false,
+    likes: { likes: "5462", increment: "2257%", decrement: false },
+  },
+  {
+    socialMedia: "Twitter",
+    retweets: 117,
+    increment: "303%",
+    decrement: false,
+    likes: { likes: "507", increment: "553%", decrement: false },
+  },
+  {
+    socialMedia: "Youtube",
+    totalViews: "1407",
+    decrement: false,
+    likes: { likes: "170", increment: false, decrement: "19%" },
+  },
 ];
 
 function App() {
-  return <Dashboard />;
+  return <Dashboard data={data} dailyIncomingData={dailyIncomingData} />;
 }
 
 export default App;
